3a2e98b1e83b2c39ff94f1d5c3304f3526063c0f
Kadeeza <dzawunor7@gmail.com>
2024-10-17 21:32:37 -0400

    Add files via upload

---
+++ static/schema.sql
@@ -0,0 +1,19 @@
+-- Create a new table
+CREATE TABLE Map_Data (
+    Lat DECIMAL,
+    Lon DECIMAL,
+    AQI INTEGER,
+    CO DECIMAL,
+    NH3 DECIMAL,
+    NO2 DECIMAL,
+    O3 DECIMAL,
+    PM10 DECIMAL,
+    "PM2.5" DECIMAL,
+    SO2 DECIMAL
+);
+
+
+
+Select * from Map_Data
+
+-- imported csv from Resource/map_data.csv into Map_Data table 
